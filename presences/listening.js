20ecbc296d1067e6cc836cd734ccec226dce3613
Mr Pumpkin <129291449+FriendlyPumpkin@users.noreply.github.com>
2023-07-06 03:06:46 +0800

    Update listening.js

--- presences/listening.js
+++ presences/listening.js
@@ -1,1 +1,59 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
 
+
+app.get('/', (req, res) => {
+    res.send('working');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
+});
+
+const Discord = require('discord.js-selfbot-v13');
+const client = new Discord.Client({
+    checkUpdate: false
+});
+
+let startTimestamp = Date.now();
+client.on('ready', () => {
+  console.log(`\nready!`);
+  updatePresence();
+});
+
+
+APPID = "123456789" 
+listeningto = "Spotify"
+song = "hallo"
+artist = "hi"
+album = "hi"
+songid = '3UnfOb5hmnf4KF7NIgAj9a?si=XSKOLBmMQqiMn8Q-eaSddA'
+
+async function updatePresence() {
+  try {
+    const r = new Discord.SpotifyRPC(client)  
+        .setApplicationId(APPID)
+        .setURL("https://www.youtube.com/watch?v=HTp5PH8ot6Q&list=RDGMEMHDXYb1_DDSgDsobPsOFxpA&ab_channel=NightLovell")
+        .setDetails(song)
+        .setName(listeningto)
+        .setState(artist)
+        
+        .setStartTimestamp(startTimestamp)
+        .setEndTimestamp(Date.now() + 1_000 * (2 * 60 + 56)) // Song length = 2m56s
+        .setAssetsLargeImage('spotify:1124634087861268492')
+        .setAssetsLargeText(album)
+        .setSongId(songid) 
+          
+    client.user.setPresence({ activities: [r.toJSON()] });
+  } catch (error) {
+    console.error('Failed :', error);
+  }
+
+  setTimeout(updatePresence, 30000); 
+      }
+
+    
+client.login(process.env.TOKEN)
+  
